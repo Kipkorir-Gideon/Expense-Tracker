2ad657fb09b900d1b771e7dacaa888d073c9ec5e
Kipkorir-Gideon <gidaliosventures@gmail.com>
2022-12-19 14:56:04 +0300

    Create three functions to handle the change in each input field using UseState hok.

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+    const [title, setTitle] = useState('');
+    const [amount, setAmount] = useState('');
+    const [date, setDate] = useState('');
+
+    const titleChangeHandler = (event) => {
+        setTitle(event.target.value);
+    }
+
+    const amountChangeHandler = (event) => {
+        setAmount(event.target.value);
+    }
+    const dateChangeHandler = (event) => {
+        setDate(event.target.value);
+    }
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0" max="0" />
+          <input type="number" min="0" max="0" onChange={amountChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2022-01-01" max="2024-12-31" />
+          <input type="date" min="2022-01-01" max="2024-12-31" onChange={dateChangeHandler} />
         </div>
       </div>
       <div className="new-expense__actions">
